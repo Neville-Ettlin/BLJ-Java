0c5c5989cb2c71605a3c6cd4d981ff7818763cc1
Urs Nussbaumer <Urs.Nussbaumer@ICT-BZ.ch>
2025-09-29 10:14:59 +0200

    Refactor.

--- Aufgabe 8_Verbotene Worte/src/Main8.java
+++ Aufgabe 8_Verbotene Worte/src/Main8.java
@@ -5,24 +5,20 @@ import java.util.Scanner;
 public class Main8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean verbotenesWortEnthalten = false;
         int verbotenesWortCounter = 0;
+
         System.out.println("Dein Kommentar: ");
-        String kommentarAnfang = scanner.nextLine();
+        String kommentar = scanner.nextLine();
 
         String[] verboteneWorte = { "viagra", "sex", "porno", "fick", "schlampe", "arsch", "mullet"};
 
-        String[] kommentarInEinzelnenWorten = kommentarAnfang.toLowerCase().split(" ");
-
-        for (int t = 0; t <= kommentarInEinzelnenWorten.length-1; t++) {
-            for (int q = 0; q <= 6; q++) {
-                if (Objects.equals(verboteneWorte[q], kommentarInEinzelnenWorten[t])) {
-                     verbotenesWortEnthalten = true;
-                     verbotenesWortCounter++;
-                    }
-                }
+        for (String s : verboteneWorte) {
+            if (kommentar.contains(s)) {
+                verbotenesWortCounter++;
             }
-        if  (verbotenesWortEnthalten) {
+        }
+
+        if  (verbotenesWortCounter > 0) {
             System.out.println("Dein Kommentar enthält " + verbotenesWortCounter + " verbotene Wörter.");
             System.out.println("Er wird nicht veröffentlicht.");
         } else {
