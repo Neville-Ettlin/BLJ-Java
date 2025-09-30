1afa215497a1c5662307fa9fca1fac0baf12914b
Neville_Ettlin <neville.ettlin@bluewin.ch>
2025-09-30 10:45:01 +0200

    Aufgaben 1 bis 17 abgesehen von Validierung gelöst.

---
+++ Aufgabe 17_Alter berechnen/src/Main17.java
@@ -0,0 +1,44 @@
+import java.time.Instant;
+import java.util.Scanner;
+
+public class Main17 {
+
+    public static String reverseGeburtsdatum (String geburtsdatum) {
+        StringBuilder reversedNameBuilder = new StringBuilder();
+        StringBuilder subNameBuilder = new StringBuilder();
+
+        for (int i = 0; i < geburtsdatum.length(); i++) {
+
+            char currentChar = geburtsdatum.charAt(i);
+
+            if (currentChar != '-') {
+                subNameBuilder.append(currentChar);
+            } else {
+                reversedNameBuilder.insert(0, currentChar + subNameBuilder.toString());
+                subNameBuilder.setLength(0);
+            }
+        }
+        return reversedNameBuilder.insert(0, subNameBuilder).toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ihr Geburtsdatum: ");
+        String geburtsdatum = scanner.nextLine();
+        geburtsdatum = geburtsdatum.replace(".", "-");
+
+        String geburtsdatumVollstandig = reverseGeburtsdatum(geburtsdatum) + "T00:00:00.000Z";
+
+        Instant instantGeburtstag = Instant.parse(geburtsdatumVollstandig);
+        long millisekundenSeitGeburtsdatum = instantGeburtstag.toEpochMilli();
+
+        Instant instantAktuell = Instant.now();
+        long millisekundenSeitAktuell = instantAktuell.toEpochMilli();
+
+        System.out.println("Alter in Jahren: " + (millisekundenSeitAktuell - millisekundenSeitGeburtsdatum) / 31536000000L);
+        System.out.println("Alter in Monaten: " + (millisekundenSeitAktuell - millisekundenSeitGeburtsdatum) / 2629746000L);
+        System.out.println("Alter in Wochen: " + (millisekundenSeitAktuell - millisekundenSeitGeburtsdatum) / 604800000L);
+        System.out.println("Alter in Tagen: " + (millisekundenSeitAktuell - millisekundenSeitGeburtsdatum) / 86400000L);
+    }
+}
